8ea1aa1bdae7d816065033f25711f53c7352cf1a
nixpig <143995476+nixpig@users.noreply.github.com>
2025-11-07 07:28:20 +0000

    test: add tests for namespace mappings

---
+++ internal/anosys/namespace_test.go
@@ -0,0 +1,19 @@
+package anosys_test
+
+import (
+	"testing"
+
+	"github.com/nixpig/anocir/internal/anosys"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNamespaceMappings(t *testing.T) {
+	assert.Len(t, anosys.NamespaceEnvs, 8)
+	assert.Len(t, anosys.NamespaceFlags, 8)
+
+	for key := range anosys.NamespaceFlags {
+		_, ok := anosys.NamespaceEnvs[key]
+
+		assert.True(t, ok, "missing namespace env for '%s'", key)
+	}
+}
