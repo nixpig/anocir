8ef8ceb7907c2538d6b6bcdf58ab98e71d83c80e
nixpig <143995476+nixpig@users.noreply.github.com>
2025-11-07 16:17:46 +0000

    test: ensure mapped capabilities are valid

---
+++ internal/anosys/capabilities_test.go
@@ -0,0 +1,14 @@
+package anosys
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/syndtr/gocapability/capability"
+)
+
+func TestCapabilitiesMapValid(t *testing.T) {
+	for _, e := range capabilities {
+		assert.Contains(t, capability.List(), e)
+	}
+}
