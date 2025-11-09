9d1b31d9dd6952df8ac4a197e4685a887c867214
nixpig <143995476+nixpig@users.noreply.github.com>
2025-11-09 07:33:13 +0000

    chore: rename anosys to platform

--- internal/platform/namespace_test.go
+++ internal/platform/namespace_test.go
@@ -3,16 +3,16 @@ package platform_test
 import (
 	"testing"
 
-	"github.com/nixpig/anocir/internal/anosys"
+	"github.com/nixpig/anocir/internal/platform"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNamespaceMappings(t *testing.T) {
-	assert.Len(t, anosys.NamespaceEnvs, 8)
-	assert.Len(t, anosys.NamespaceFlags, 8)
+	assert.Len(t, platform.NamespaceEnvs, 8)
+	assert.Len(t, platform.NamespaceFlags, 8)
 
-	for key := range anosys.NamespaceFlags {
-		_, ok := anosys.NamespaceEnvs[key]
+	for key := range platform.NamespaceFlags {
+		_, ok := platform.NamespaceEnvs[key]
 
 		assert.True(t, ok, "missing namespace env for '%s'", key)
 	}
